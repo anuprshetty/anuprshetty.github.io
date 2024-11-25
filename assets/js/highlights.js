1ee5c19c06dd401a080d795e9b1c77c456fbbebb
anuprshetty <anuprshetty@gmail.com>
2024-11-25 17:13:05 +0530

    updated highlights

--- assets/js/highlights.js
+++ assets/js/highlights.js
@@ -21,8 +21,38 @@ const research = [
     absbox: "absPopup1",
   },
 ];
+
+const awardswiper = document.querySelector(".swiper-wrapper");
+const award = [
+  // eslint-disable-next-line linebreak-style
+  {
+    title: "Recognition - Philips",
+    image: "assets/images/highlights-page/philips_ownership_award.jpeg",
+  },
+  {
+    title: "Hackathon - Philips",
+    image: "assets/images/highlights-page/philips_team_up_award.jpeg",
+  },
+  {
+    title: "Finalist - Graduation Project",
+    image: "assets/images/highlights-page/chandassu_award.jpeg",
+  },
+  {
+    title: "Won Coding Event - Graduation",
+    image: "assets/images/highlights-page/jss_bug_c_award.jpeg",
+  },
+  {
+    title: "Competitive Coding Workshop",
+    image: "assets/images/highlights-page/jss_workshop.jpeg",
+  },
+  // {
+  //   title: "Yoga Participation",
+  //   image: "assets/images/highlights-page/yoga_participation.jpeg",
+  // },
+];
+
 AOS.init();
-const fillData = () => {
+const researchFillData = () => {
   let output = "";
   research.forEach(
     ({
@@ -87,4 +117,85 @@ const fillData = () => {
   );
   researchTable.innerHTML = output;
 };
-document.addEventListener("DOMContentLoaded", fillData);
+document.addEventListener("DOMContentLoaded", researchFillData);
+
+function awardFillData() {
+  let output = "";
+  for (let x = 0; x < award.length; x++) {
+    output += `
+  <div class="swiper-slide">
+  <div class="img-container">
+    <img src=${award[x].image} alt="" class="img-fluid" />
+  </div>
+  <div class="container arrow_contain">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="wrapper">
+          <div class="content">
+            <div class="title">
+              <div class="text-wrapper">
+                <div class="text-inner">
+                ${award[x].title}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+       </div> 
+        <!-- wraper freee -->
+    </div>
+  </div>
+</div>`;
+  }
+  awardswiper.innerHTML = output;
+}
+document.addEventListener("DOMContentLoaded", awardFillData());
+
+const parallaxSliderOptions = {
+  speed: 1000,
+  effect: "coverflow",
+  autoplay: false,
+  parallax: true,
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 30,
+    depth: 20,
+    stretch: 0,
+    modifier: 1,
+    slideShadows: true,
+  },
+  // parallax effect
+  on: {
+    init: function () {
+      const swiper = this;
+      for (let i = 0; i < swiper.slides.length; i++) {
+        $(swiper.slides[i])
+          .find(".img-container")
+          .attr({
+            "data-swiper-parallax": 0.75 * swiper.width,
+            "data-swiper-paralalx-opacity": 0.5,
+          });
+      }
+      const index = swiper.activeIndex;
+      $(swiper.slides).removeClass("active");
+      $(swiper.slides[index]).addClass("active");
+    },
+    transitionEnd: function () {
+      const swiper = this;
+      const index = swiper.activeIndex;
+      $(swiper.slides).removeClass("active");
+      $(swiper.slides[index]).addClass("active");
+    },
+    resize: function () {
+      this.update();
+    },
+  },
+  navigation: {
+    nextEl: ".image-slider .next-ctrl",
+    prevEl: ".image-slider .prev-ctrl",
+  },
+};
+// initializing swiper
+const slider = new Swiper(".image-slider", parallaxSliderOptions);
