18d22498dce3cd077cc329efd5f01acf550a9365
anuprshetty <anuprshetty@gmail.com>
2024-12-19 10:57:16 +0530

    added resume

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,6 +27,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://drive.google.com/drive/folders/1BThrkgQSBIFlNKkKMXHcf6PuhYbxGqF6" target="_blank">Resume</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="contact.html">Hire Me</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
